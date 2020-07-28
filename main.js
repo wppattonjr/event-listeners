bc81ee27d1192ca94f7712ee4dd00352ac3b6c5d
Wendell <wendell.pattonjr@gmail.com>
2020-07-27 19:02:04 -0500

    went through in class  event listener practice

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log('Yo!');
+/* "use strict"
+
+const helloMyFriend = (friendName) => {
+
+    return `Hello, how are you doing ${friendName}?`;
+
+    
+}
+
+console.log(helloMyFriend('Carla'));
+
+const goodbyeMyFriend = (name) => {
+
+    return `Goodbye, ${name}`; 
+}
+
+console.log(goodbyeMyFriend('Carla'));
+
+document.getElementById("main-btn");
+
+const butnPractice = document.querySelector('#main-btn');
+console.log(butnPractice);
+
+const butnPracticeAgain = document.querySelector('#main-btn');
+
+butnPracticeAgain.addEventListener(click, () => {
+    console.log('mouse has entered!');
+});
+ */
+/* function clicked() {
+    return ('You clicked me!')
+}
+console.log(clicked())
+
+const div = document.querySelector('#main-btn');
+div.addEventListener(click, () => {
+    console.log
+} */
